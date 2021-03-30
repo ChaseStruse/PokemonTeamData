c1613b3cea24f68b118e95270c1706841a198dc7
Chase Struse <chasestruse@gmail.com>
2021-03-29 23:43:05 -0500

    Base commit with base stuff

--- PokemonTeamData/Program.cs
+++ PokemonTeamData/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using PokemonTeamData.Repository;
 namespace PokemonTeamData
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            ApiClient apiClient = new ApiClient();
+
+            Uri uri = new Uri("https://pokeapi.co/api/v2/pokemon/charmander");
+            var pokemon = await apiClient.GetPokemon(uri);
+
+            Console.WriteLine("Pokemon ID: " + pokemon.Id);
+            Console.WriteLine("Pokemon Name: " + pokemon.Name);
+            Console.WriteLine("Pokemon Types: " + pokemon.Types);
+            Console.WriteLine("Pokemon Abilities: " + pokemon.Abilities);
         }
+
     }
 }
