d5230acfeb97998fa04efe6d25555b1ac12ff995
Chase Struse <chasestruse@gmail.com>
2021-03-30 00:09:27 -0500

    Made printing the pokemons information better

--- PokemonTeamData/Program.cs
+++ PokemonTeamData/Program.cs
@@ -3,6 +3,8 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using PokemonTeamData.Repository;
+using PokemonTeamData.Repository.Models;
+
 namespace PokemonTeamData
 {
     class Program
@@ -12,13 +14,34 @@ namespace PokemonTeamData
             Program program = new Program();
             ApiClient apiClient = new ApiClient();
 
-            Uri uri = new Uri("https://pokeapi.co/api/v2/pokemon/charmander");
+            Uri uri = new Uri("https://pokeapi.co/api/v2/pokemon/charizard");
             var pokemon = await apiClient.GetPokemon(uri);
+            program.printPokemon(pokemon);
+        }
 
+        private void printPokemon(Pokemon pokemon)
+        {
             Console.WriteLine("Pokemon ID: " + pokemon.Id);
             Console.WriteLine("Pokemon Name: " + pokemon.Name);
-            Console.WriteLine("Pokemon Types: " + pokemon.Types);
-            Console.WriteLine("Pokemon Abilities: " + pokemon.Abilities);
+            Console.WriteLine("");
+            Console.WriteLine("Types: ");
+            foreach (var pokemonType in pokemon.Types)
+            {
+                Console.WriteLine(pokemonType.Name);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Abilities:");
+            foreach (var pokemonAbility in pokemon.Abilities)
+            {
+                Console.WriteLine(pokemonAbility.Name);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Base Stats:");
+            foreach (var pokemonBaseStats in pokemon.Stats)
+            {
+                Console.WriteLine(pokemonBaseStats.Name);
+                Console.WriteLine(pokemonBaseStats.BaseValue);
+            }
         }
 
     }
