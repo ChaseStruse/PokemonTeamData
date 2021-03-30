f97abb144c5e8828afd3409ede10f7d4e1116cf1
Chase Struse <chasestruse@gmail.com>
2021-03-30 09:47:28 -0500

    Began testing service layer

---
+++ PokemonTeamData.Service.Tests/PokemonServiceTests.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace PokemonTeamData.Service.Tests
+{
+    public class PokemonServiceTests
+    {
+        private readonly PokemonService _sut;
+
+        public PokemonServiceTests()
+        {
+            _sut = new PokemonService();
+        }
+
+        [Fact]
+        public void GivenValidPokemonName_UriBuilder_CreatesAndReturnsValidUri()
+        {
+            Uri expected = new Uri("https://pokeapi.co/api/v2/pokemon/charmander");
+            var actual = _sut.UriBuilder("charmander");
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ PokemonTeamData.Service/PokemonService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace PokemonTeamData.Service
+{
+    public interface IPokemonService
+    {
+        Uri UriBuilder(string pokemonName);
+    }
+
+    public class PokemonService
+    {
+        public PokemonService()
+        {
+        }
+
+        public Uri UriBuilder(string pokemonName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
